5c8e6393efaecac0191fdaddff00efbefd03775a
yonatanlla <yonatanllanto174@gmail.com>
2023-09-22 17:49:30 -0500

    Ejercicios terminado

--- listas/elem_grande.py
+++ listas/elem_grande.py
@@ -1,11 +1,23 @@
 # Ejercicio 2: Encontrar el elemento más grande en una lista
 # Escribe una función que encuentre el elemento más grande en una lista de números.
 
-elemt = [2,3,5,1,5,6,8]
+elemt = [2,3,5,12,1,5,6,8]
+
+result = None
 
 def elemento(elemt):
-    print(elemt)
+    result = None
+    for elem in elemt:
+        if result is None or elem > result:
+            result = elem
+    return result            
+print(elemento(elemt))    
 
+# DE UNA FORMA MAS SENCILLA
 
-elemento(elemt)
-    
+# def newElemnt(elemt):
+#     return max(elemt)
+
+# resultado = newElemnt(elemt)
+
+# print(resultado)
